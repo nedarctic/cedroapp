aa10162b055ea47e8b83f66d9a95db5211da2c57
Ned Arctic <nedarctic@gmail.com>
2026-08-10 20:10:29 +0300

    get dynamic destination names for filters

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -3,6 +3,7 @@ import { PaginationComponent } from "@/components/pagination";
 import { PopularTourCard } from "@/components/popular-tour-card";
 import { Search } from "@/components/search";
 import { SectionHeadline } from "@/components/section-headline";
+import { getDestinationNames } from "@/lib/helpers/destinations.helpers";
 import { getTours } from "@/lib/helpers/tours.helpers";
 
 export default async function Tours({ searchParams }: {
@@ -27,10 +28,12 @@ export default async function Tours({ searchParams }: {
     search && params.append("search", search);
     filter && params.append("filter", filter);
 
-    const url = `${process.env.BACKEND_URL}/tours?${params.toString()}`;
+    const url = `${process.env.BACKEND_URL}/tours/paginated-tours?${params.toString()}`;
 
     const { data } = await getTours(url);
     const { meta, tours } = data!;
+
+    const { data: destinationNames } = await getDestinationNames();
 
     return (
         <main className="flex flex-col justify-center items-center py-8 sm:py-12 md:py-16 lg:py-24 bg-white gap-6">
@@ -45,38 +48,35 @@ export default async function Tours({ searchParams }: {
 
             {/* filters - responsive padding and width */}
             <div className="w-full px-4 sm:px-6 md:px-8 lg:px-20 self-start">
-                <Filters filters={["Kenya", "Tanzania", "Rwanda", "Zanzibar"]} />
+                <Filters filters={destinationNames!} />
             </div>
 
             {/* grid containing tour cards */}
             <div className="w-full px-4 lg:w-11/12 flex flex-col justify-center items-center">
                 {tours.length > 0 ? (
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 md:gap-8 lg:gap-10 w-full px-4 sm:px-0">
-                        {tours.map(({ dates,
+                        {tours.map(({
+                            dates,
                             groupSize,
                             destination,
                             duration,
-                            tourImageUrl,
+                            tourImage,
                             price,
                             title,
                             id,
 
-                        }) => {
-
-                            const formattedPrice = Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(Number(price))
-
-                            return <PopularTourCard
+                        }) => <PopularTourCard
                                 key={title}
                                 dates={dates}
                                 group_size={groupSize}
                                 destination={destination?.name!}
                                 duration={duration}
-                                image={tourImageUrl}
-                                price={formattedPrice}
+                                image={tourImage}
+                                price={price}
                                 title={title}
                                 link={`/tours/${id}`}
                             />
-                        })}
+                        )}
                     </div>
                 ) : (
                     <p className="font-bold text-black text-md sm:text-lg text-center px-4">

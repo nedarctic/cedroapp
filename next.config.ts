f405dfaa2aba172a328dfe2d808f09b4ab7a48a2
Ned Arctic <nedarctic@gmail.com>
2026-08-10 17:19:27 +0300

    configure new hostname and protocol for remote images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         hostname: "pub-5cee8e6d1a574b6c84697dfdb9beba4a.r2.dev",        
+      },
+      {
+        hostname: "pub-79ed953562964dbfa4ff96ef322c18ac.r2.dev",
+        protocol: "https",
       }
     ]
   }

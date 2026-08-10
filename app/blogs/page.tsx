2de18b581f779cd3a8eae2e6791ad10090ae7ae1
Ned Arctic <nedarctic@gmail.com>
2026-08-11 01:50:30 +0300

    param handling

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,21 +1,41 @@
 import { NewsletterSignup } from "@/components/newsletter-signup";
+import { PaginationComponent } from "@/components/pagination";
 import { Search } from "@/components/search";
 import { SectionHeadline } from "@/components/section-headline";
 import { TravelStoryCard } from "@/components/travel-story-card";
-import { getBlogs } from "@/lib/helpers";
+import { getBlogs } from "@/lib/helpers/blogs.helpers";
 import Link from 'next/link';
 
 export default async function Blogs({ searchParams }: {
-    searchParams: Promise<{ query: string }>
+    searchParams: Promise<{
+        page?: string;
+        limit?: string;
+        search?: string;
+    }>
 }) {
 
-    const { query } = await searchParams;
-    const blogs = await getBlogs(query);
+    const {
+        limit = "10",
+        page = "1",
+        search
+    } = await searchParams;
+
+    const params = new URLSearchParams();
+
+    params.append("page", page);
+    params.append("limit", limit);
+    search && params.append("search", search);
+
+    const url = `${process.env.BACKEND_URL}/blogs?${params.toString()}`
+
+    const { data: paginatedBlogsData } = await getBlogs(url);
+    const { blogs, meta } = paginatedBlogsData!;
+    console.log("blogs", blogs);
 
     return (
         <main className="min-h-screen flex flex-col items-center bg-white w-full">
-            <section className="flex flex-col items-center justify-start w-full bg-white py-8 sm:py-12 md:py-16 lg:py-20 px-4 sm:px-6 md:px-8">
-                
+            <section className="flex flex-col items-center justify-start w-full bg-white py-8 sm:py-12 md:py-16 lg:py-20 px-4 sm:px-6 md:px-8 gap-6">
+
                 {/* travel blog headline */}
                 <SectionHeadline title={"Our travel blogs"} color={"black"} />
 
@@ -28,17 +48,17 @@ export default async function Blogs({ searchParams }: {
                 <div className="flex flex-col justify-center items-center w-full max-w-7xl mt-8 sm:mt-12 md:mt-16 lg:mt-20">
                     {blogs.length > 0 ? (
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-6 lg:gap-8 w-full">
-                            {blogs?.map(({ title, image, excerpt, date, slug }) => (
-                                <Link 
-                                    key={slug} 
-                                    href={`/blogs/${slug}`}
+                            {blogs?.map(({ title, blogImage, excerpt, date, id }) => (
+                                <Link
+                                    key={id}
+                                    href={`/blogs/${id}`}
                                     className="block transition-transform hover:scale-[1.02] duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 rounded-lg"
                                 >
-                                    <TravelStoryCard 
-                                        title={title} 
-                                        image={image} 
-                                        date={date} 
-                                        story={excerpt} 
+                                    <TravelStoryCard
+                                        title={title}
+                                        image={blogImage}
+                                        date={date}
+                                        story={excerpt}
                                     />
                                 </Link>
                             ))}
@@ -54,6 +74,8 @@ export default async function Blogs({ searchParams }: {
                         </div>
                     )}
                 </div>
+
+                <PaginationComponent meta={meta} />
 
                 {/* newsletter signup component - responsive width */}
                 <div className="w-full mt-8 sm:mt-12 md:mt-16 lg:mt-20">

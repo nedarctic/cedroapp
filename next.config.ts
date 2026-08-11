e5169b66ee5360fee9a6d516c651716ff5b83334
Ned Arctic <nedarctic@gmail.com>
2026-08-11 13:59:25 +0300

    unused hostname rid

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "pub-5cee8e6d1a574b6c84697dfdb9beba4a.r2.dev",        
-      },
-      {
         hostname: "pub-79ed953562964dbfa4ff96ef322c18ac.r2.dev",
         protocol: "https",
       }

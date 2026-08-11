12a1297344f9dfcbc1feec4228cd12633b570ba5
Ned Arctic <nedarctic@gmail.com>
2026-08-11 13:58:12 +0300

    booking api route handler

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,14 +1,36 @@
+import { CustomBookTourForm } from "@/components/custom-book-tour-form";
 import { SectionHeadline } from "@/components/section-headline";
 import { TourCard } from "@/components/tour-card";
-import { tours } from "@/lib/data";
+import { getAllTours } from "@/lib/helpers/tours.helpers";
 
-export default function ToursPage() {
+export default async function ToursPage() {
+
+  const { data: tours } = await getAllTours();
+
   return (
     <main className="min-h-screen w-full flex flex-col items-center bg-white overflow-x-hidden">
-      
-      {/* hero / intro section */}
+
+      {/* tour booking request */}
+      <section className="flex flex-col items-center justify-center py-12 sm:py-16 md:py-20 lg:py-24 w-full px-4 sm:px-6 md:px-8 gap-6">
+
+        <SectionHeadline
+          title="Tour Booking Request"
+          color="black"
+        />
+
+        <p className="text-black text-base sm:text-lg md:text-xl lg:text-2xl font-normal w-full sm:w-3/4 lg:w-1/2 text-center pt-6 sm:pt-8 md:pt-10 px-4 leading-relaxed">
+          Submit a booking request using your name and email address for quick assistance with your tour experience from our team.
+        </p>
+
+        <CustomBookTourForm />
+
+      </section>
+
+      {/* select a tour */}
       <section className="flex flex-col items-center justify-center py-12 sm:py-16 md:py-20 lg:py-24 w-full px-4 sm:px-6 md:px-8">
-        
+
+
+
         <SectionHeadline
           title="Our Safari Tours"
           color="black"
@@ -23,19 +45,19 @@ export default function ToursPage() {
 
       {/* tours grid section */}
       <section className="w-full flex flex-col items-center pb-16 sm:pb-20 md:pb-24 lg:pb-32 px-4 sm:px-6 md:px-8">
-        
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 md:gap-10 lg:gap-12 w-full max-w-7xl">
-          
-          {tours.map((tour) => (
+
+          {tours?.map((tour) => (
             <TourCard
-              key={tour.slug}
+              key={tour.id}
               title={tour.title}
               intro={tour.intro}
               duration={tour.duration}
-              group_size={tour.group_size}
+              group_size={tour.groupSize}
               price={tour.price}
-              heroImage={tour.heroImage}
-              slug={tour.slug}
+              heroImage={tour.tourImage}
+              slug={tour.id}
             />
           ))}
         </div>

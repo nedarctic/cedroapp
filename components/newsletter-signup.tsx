86742f2631f686177df9a7cf02ae5d230509cd9f
Ned Arctic <nedarctic@gmail.com>
2026-08-13 23:46:37 +0300

    subscription handler

--- components/newsletter-signup.tsx
+++ components/newsletter-signup.tsx
@@ -1,42 +1,70 @@
 "use client"
 
-import { SubscriptionAction } from "@/actions/subscription.actions";
-import { useState, useActionState, useEffect } from "react";
-import { StatusDialog } from "./status-dialog";
+import { useState, SubmitEvent } from "react";
+import { toast } from "sonner";
+import z from "zod";
 
 export function NewsletterSignup() {
 
     const [email, setEmail] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const prevState = {
-        error: undefined,
-        success: false
-    };
+    const submitHandler = async (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    const [state, formAction, pending] = useActionState(SubscriptionAction, prevState)
+        try {
+            setLoading(true);
 
-    const [open, setOpen] = useState<boolean>(false);
+            const validationResult = z.email("Invalid email").safeParse(email);
+            if (!validationResult.success) {
+                setLoading(false);
+                toast.error("Subscription failed.", { description: "Invalid email" });
+                return;
+            }
 
-    useEffect(() => {
-        setOpen(true)
-    }, [state.success])
-    
+            const url = "/api/subscriptions";
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({email})
+            });
+
+            const { success } = await res.json();
+            if(!res.ok || !success){
+                setLoading(false);
+                toast.error("Subscription failed. Please try again later.")
+                return;
+            }
+
+            setLoading(false);
+            setEmail("");
+            toast.success("Subscription successful");
+            return;
+
+        } catch (error) {
+            toast.error("Service currently unavailable. Please try again later.");
+            setLoading(false);
+            return;
+        }
+    }
+
     return (
         <div className="flex flex-col justify-center items-center my-40">
             <p className="text-black text-md font-normal">Sign up to our newsletter for updates</p>
-            <form action={formAction} className="flex flex-row items-center justify-center gap-4 mt-4">
-                <input 
-                type="email" 
-                required 
-                name="email"
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-60 px-4 py-2 border border-black placeholder:text-gray-500" 
-                placeholder="email@gmail.com" />
+            <form onSubmit={submitHandler} className="flex flex-row items-center justify-center gap-4 mt-4">
+                <input
+                    type="email"
+                    required
+                    value={email}
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-60 px-4 py-2 border border-black placeholder:text-gray-500"
+                    placeholder="email@gmail.com" />
 
-                <button className="px-4 py-2 text-black bg-[#FAD039]">{pending ? "Signing up..." : "Sign up"}</button>
+                <button className="px-4 py-2 text-black bg-[#FAD039]">{loading ? "Signing up..." : "Sign up"}</button>
             </form>
-            {state.error?.includes("Unique constraint failed") && <StatusDialog message="Email already subscribed" error={true} open={open} onOpenChange={setOpen} />}
-            {state.success && <StatusDialog message="Email successfully subscribed!" error={false} open={open} onOpenChange={setOpen} />}
         </div>
     );
 }

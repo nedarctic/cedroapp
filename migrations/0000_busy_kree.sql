d2cfbee16e19804ff804e955ad40d82a8fd768bd
Ned Arctic <nedarctic@gmail.com>
2026-08-27 01:48:30 +0300

    removed prisma

--- migrations/0000_busy_kree.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "bookings" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"tour_title" text NOT NULL,
-	"name" text NOT NULL,
-	"email" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
